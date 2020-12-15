f2fe16cc4513e1da163634f65bd015f6411196bf
Basir-Payenda <basirrpayenda@gmail.com>
2020-12-15 14:23:53 +0500

    Added 5th and 6th Projects (Modal & Accordion)

---
+++ 6 - How To Create a Modal Using HTML, CSS and JavaScript/script.js
@@ -0,0 +1,26 @@
+const btns = document.querySelectorAll("[data-target]");
+const close_modals = document.querySelectorAll(".close-modal");
+const overlay = document.getElementById("overlay");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(btn.dataset.target).classList.add("active");
+    overlay.classList.add("active");
+  });
+});
+
+close_modals.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const modal = btn.closest(".modal");
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+});
+
+window.onclick = (event) => {
+  if (event.target == overlay) {
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach((modal) => modal.classList.remove("active"));
+    overlay.classList.remove("active");
+  }
+};
---
+++ 7 - How To Create an Accordion/script.js
@@ -0,0 +1,27 @@
+const acc_btns = document.querySelectorAll(".accordion-header");
+const acc_contents = document.querySelectorAll(".accordion-body");
+
+acc_btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    acc_contents.forEach((acc) => {
+      if (
+        e.target.nextElementSibling !== acc &&
+        acc.classList.contains("active")
+      ) {
+        acc.classList.remove("active");
+        acc_btns.forEach((btn) => btn.classList.remove("active"));
+      }
+    });
+
+    const panel = btn.nextElementSibling;
+    panel.classList.toggle("active");
+    btn.classList.toggle("active");
+  });
+});
+
+window.onclick = (e) => {
+  if (!e.target.matches(".accordion-header")) {
+    acc_btns.forEach((btn) => btn.classList.remove("active"));
+    acc_contents.forEach((acc) => acc.classList.remove("active"));
+  }
+};
